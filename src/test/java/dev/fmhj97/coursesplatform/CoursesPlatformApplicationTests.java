0706adc14637dc564badff644b26d89211c08717
Francisco Manuel Hernández <francisco-h-j@hotmail.com>
2026-04-18 19:51:23 +0200

    feat: initial project commit

---
+++ src/test/java/dev/fmhj97/coursesplatform/CoursesPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fmhj97.coursesplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
